96cfcad45546459327b5f8e851d457581d300da8
tntuan0712494 <tntuan0712494@562a60f5-e086-70c4-b18a-a5cb1e411a35>
2010-08-08 16:53:29 +0000

    đọc và ghi được file excel, hàm được implement trong lớp FilterBadData - CreateDataTableAndData

---
+++ PROJ_JES_001/FilterBadData/src/mainclass.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+import javax.swing.*;    
+public class mainclass {   
+	public static void main( String[] args ) throws IOException {     
+		FilterBadData fbd = new FilterBadData();
+		fbd.RunApplication();
+		} 
+	}

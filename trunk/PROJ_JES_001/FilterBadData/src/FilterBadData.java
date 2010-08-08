cf3e19eae3365bfff2ac5d7084ca0b20628968d2
tntuan0712494 <tntuan0712494@562a60f5-e086-70c4-b18a-a5cb1e411a35>
2010-08-08 16:53:29 +0000

    đọc và ghi được file excel, hàm được implement trong lớp FilterBadData - CreateDataTableAndData

--- trunk/PROJ_JES_001/FilterBadData/src/FilterBadData.java
+++ trunk/PROJ_JES_001/FilterBadData/src/FilterBadData.java
@@ -1,3 +1,14 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.List;
+
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.hssf.usermodel.*;
+
 
 public class FilterBadData {
 	private ExcelFile m_FileIn; 
@@ -5,13 +16,27 @@ public class FilterBadData {
 	private FunctionManager m_ListFunction; 
 	private DataTable m_DBAllCell; 
 	
-	public void RunApplication()
+	public void RunApplication() throws IOException
 	{
-		
+		CreateDataTableAndData();
 	}
-	private void CreateDataTableAndData()
+	private void CreateDataTableAndData() throws IOException
 	{
-		
+		InputStream inp = new FileInputStream("LDH.xls");
+		Workbook wb = new HSSFWorkbook(inp);
+	    Sheet sheet = wb.getSheetAt(0);
+	    Row row = sheet.getRow(2);
+	    Cell cell = row.getCell(3);
+	    if (cell == null)
+	        cell = row.createCell(3);
+	    cell.setCellType(Cell.CELL_TYPE_STRING);
+	    cell.setCellValue("TNT");
+
+	    // Write the output to a file
+	    FileOutputStream fileOut = new FileOutputStream("LDH.xls");
+	    wb.write(fileOut);
+	    fileOut.close();
+
 	}
 	private void RunFilter()
 	{
